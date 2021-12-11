b7578910801a64f0f885bf05e20b5861d6721f17
Hiro-1978 <78284161+Hiro-1978@users.noreply.github.com>
2021-12-11 17:11:33 +0700

    springboot-first-app
    15 - Create Spring Boot REST API returns List

--- springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
+++ springboot-first-app/src/main/java/com/springboot/first/app/StudentController.java
@@ -3,11 +3,25 @@ package com.springboot.first.app;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     // http://localhost:8080/student
     @GetMapping("/student")
     public Student getStudent(){
         return new Student("Paisan","Pompadech");
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Nick","Fury"));
+        students.add(new Student("Clint","Barton"));
+        students.add(new Student("Natasha","Romanov"));
+        students.add(new Student("Stive","Roger"));
+        students.add(new Student("Tony","Stark"));
+        return students;
     }
 }
